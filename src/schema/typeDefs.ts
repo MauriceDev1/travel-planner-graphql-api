81554ee609ae3470730dc6dc158fc709c2c27467
mauricevolkwyn0-bit <mauricevolkwyn0@gmail.com>
2026-02-07 19:08:57 +0200

    feat: add GraphQL schema for weather and activities

--- src/schema/typeDefs.ts
+++ src/schema/typeDefs.ts
@@ -1,6 +1,56 @@
 const typeDefs = `#graphql
+  type City {
+    id: ID!
+    name: String!
+    country: String!
+    latitude: Float!
+    longitude: Float!
+    region: String
+  }
+
+  type Weather {
+    temperature: Float!
+    conditions: String!
+    precipitation: Float!
+    windSpeed: Float!
+    humidity: Float
+    timestamp: String!
+  }
+
+  type WeatherForecast {
+    city: City!
+    current: Weather!
+    forecast: [Weather!]!
+  }
+
+  type Activity {
+    name: String!
+    score: Float!
+    recommended: Boolean!
+    reason: String!
+  }
+
+  type ActivityRanking {
+    city: City!
+    weather: Weather!
+    activities: [Activity!]!
+  }
+
   type Query {
-    hello: String
+    """
+    Search for cities by name (minimum 2 characters)
+    """
+    searchCities(query: String!): [City!]!
+
+    """
+    Get weather forecast for a specific city
+    """
+    getWeatherForecast(cityId: String!): WeatherForecast
+
+    """
+    Get activity recommendations based on current weather
+    """
+    getActivityRankings(cityId: String!): ActivityRanking
   }
 `;
 
